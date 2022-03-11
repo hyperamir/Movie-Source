80234ff0314644eac8cd10cb91e3d36b488c0778
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-11 09:18:01 -0500

    import proptypes and use it

--- src/components/Actor/index.js
+++ src/components/Actor/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Wrapper, Image } from "./Actor.styles";
+import PropTypes from 'prop-types'
 
 const Actor = (props) => {
+  const {imageUrl,name,character} = props
   return (
     <Wrapper>
-      <Image src={props.imageUrl} alt="actor-thumb" />
-      <h3>{props.name}</h3>
-      <p>{props.character}</p>
+      <Image src={imageUrl} alt="actor-thumb" />
+      <h3>{name}</h3>
+      <p>{character}</p>
     </Wrapper>
   );
+}
+Actor.propTypes = {
+  imageUrl: PropTypes.string,
+  name: PropTypes.string,
+  character: PropTypes.string
 }
 
 export default Actor;
