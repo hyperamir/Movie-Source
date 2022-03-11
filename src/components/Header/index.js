24fb44936936a02ecdbc2b4e13de5084defcf4ed
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-11 15:01:29 -0500

    design logo svg

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import RMDBLogo from '../../images/react-movie-logo.svg'
-import TMDBLogo from '../../images/tmdb_logo.svg'
+import RMDBLogo from '../../images/logo.svg';
+import TMDBLogo from '../../images/tmdb_logo.svg';
 
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 
